a738429e07ff3b5d1c231db115666d97e774a960
Jaimin1312 <61569981+Jaimin1312@users.noreply.github.com>
2020-10-21 23:36:22 +0530

    Update settings.py

--- AutoGradingSystem/settings.py
+++ AutoGradingSystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+#nedlsjaxafqmlnms
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'autogradingsystem99999@gmail.com'
-EMAIL_HOST_PASSWORD = 'nedlsjaxafqmlnms'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
